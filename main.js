ebca798c56888f7aab16855c41484a7f1fa16bee
Dan Chrostowski <dan@danchrostowski.com>
2021-02-24 19:16:27 -0700

    remove screenshots, clean up console.logs

--- main.js
+++ main.js
@@ -3,8 +3,6 @@ var fs = require('fs')
 const util = require('util');
 const writeFile = util.promisify(fs.writeFile)
 
-
-
 const waitAFewSeconds = async () => {
   return new Promise((resolve) => {
     setTimeout(() => {
@@ -35,30 +33,21 @@ const main = async () => {
   await page.goto('https://play.swcpoker.club', { waitUntil: 'networkidle0', timeout: 60000 })
 
   const [signin, forgot, signup, cancel] = await page.$x('//div[@class="simple-button-content"]')
-  await page.screenshot({ path: 'swc1.png' })
-
+  
   await cancel.click()
   await waitAFewSeconds()
 
-  //await page.waitForNavigation({ waitUntil: 'networkidle2', timeout: 60000 }),
-
-  await page.screenshot({ path: 'swc2.png' })
-
   const [lobby_div] = await page.$x('//div[@class="navigation-panel-back-content"]')
-  console.log(lobby_div)
 
   await lobby_div.click()
-  //await page.waitForNavigation({ waitUntil: 'networkidle2' }),
+  
   await waitAFewSeconds()
-  await page.screenshot({ path: 'swc3.png' })
 
 
   const [tournaments_btn] = await page.$x('//div[@class="menu-item-content" and text()="Tournaments"]')
   await tournaments_btn.click()
   await waitAFewSeconds()
-  //await page.waitForNavigation({ waitUntil: 'networkidle2' }),
 
-  await page.screenshot({ 'path': 'swc4.png' })
 
   const runningDivs = await page.$x('//div[@class="panel tournament-line running"]')
   const latRegDivs = await page.$x('//div[@class="panel tournament-line late-registration running"]')
@@ -82,15 +71,11 @@ const main = async () => {
     if (jsonMatch) {
       jsonData1 = JSON.parse(jsonMatch[1])
       jsonData2 = JSON.parse(jsonData1[1])
-      console.log("DATA: ")
-      console.log(jsonData2)
       if (jsonData2.hasOwnProperty('t') && jsonData2['t'] === 'LobbyTournamentInfo') {
         let tourneyName = jsonData2.info.n
         if (!this.tournamentAndPlayers.hasOwnProperty(tourneyName)) {
           this.tournamentAndPlayers[tourneyName] = {}
         }
-
-
 
         let rawPlayers = jsonData2.players
 
@@ -107,10 +92,7 @@ const main = async () => {
             place = place + 1
 
             this.tournamentAndPlayers[tourneyName][playerName] = { 'position': place, 'chips': chips, 'expectedPosition': expectedPlace, 'playerRating': playerRate, 'isPro': isPro }
-
           }
-
-
         })
 
 
@@ -122,23 +104,18 @@ const main = async () => {
   cdp.on('Network.webSocketFrameSent', printRequest);
 
   for (let i = 0; i < tournamentDivs.length; i++) {
-    console.log("getting tournament info " + i + " of " + tournamentDivs.length)
     let refreshRunning = await page.$x('//div[@class="panel tournament-line running"]')
     let refreshLateReg = await page.$x('//div[@class="panel tournament-line late-registration running"]')
     let refreshDivs = refreshRunning.concat(refreshLateReg)
     let div = refreshDivs[i]
-    console.log("clicking on tournament div")
     await div.click()
     await waitFor(10000)
     let [backButton] = await page.$x('//div[@class="navigation-panel-back-content"]')
     await backButton.click()
-    console.log("SCRAPED:")
-    console.log(this.tournamentAndPlayers)
     await waitFor(3000)
 
   }
-  console.log("END")
-  console.log(this.tournamentAndPlayers)
+  
   let tournamentKeys = Object.keys(this.tournamentAndPlayers)
   sortedRankings = {}
 
@@ -149,15 +126,13 @@ const main = async () => {
     for (let j = 0; j < playerKeys.length; j++) {
       let playerData = currTournament[playerKeys[j]]
       playerData['playerName'] = playerKeys[j]
-      unsorted.push(playerData)
+      simplifiedPlayerData = {'playerName': playerKeys[j], 'position': playerData.position, 'chips': playerData.chips}
+      unsorted.push(simplifiedPlayerData)
     }
     let sorted = unsorted.sort((a, b) => (a.position > b.position ? 1 : -1))
     sortedRankings[tournamentKeys[i]] = sorted
 
   }
-
-
-  console.log(sortedRankings)
 
   await writeFile('./tournamentRankings.json', JSON.stringify(this.tournamentAndPlayers))
   await writeFile('./sortedRankings.json', JSON.stringify(sortedRankings))
